4e64e4b7d1fc77278efe4c76ed2bf52f65445e90
Travis Talcott <ttalcott@lyradevelopment.com>
2017-02-25 09:10:14 -0700

    Added enrollment and course models

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        [Display(Name = "Course Title")]
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
